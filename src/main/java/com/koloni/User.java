4740588f816cf91e70f8e0d26ceb52a86a65d9cd
Adam <adsal92@hotmail.com>
2024-06-19 13:09:53 +0200

    Initial commit

---
+++ src/main/java/com/koloni/User.java
@@ -0,0 +1,19 @@
+package com.koloni;
+
+public class User {
+    private String id;
+
+    public User(String id, String password) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+}
